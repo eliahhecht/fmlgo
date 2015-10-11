3dcd04e5961d652fd055b8253cbb402805e613e9
Eliah Hecht <eliahhecht@gmail.com>
2015-10-11 08:38:39 -0400

    move printing code to its own file

---
+++ main/printing.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"text/tabwriter"
+	"os"
+	"io"
+	"sort"
+)
+
+func outputScores(players []*Player, allCards *CardCollection) {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+
+	for _, player := range players {
+		fmt.Fprintf(w, "== %s: \t%d ==\n", player.Name, player.TotalScore())
+		printCardScoresForPlayer(w, player.Cards)
+		fmt.Fprintln(w, "  Bench: \t")
+		printCardScoresForPlayer(w, player.Bench)
+		fmt.Fprintln(w, "\t")
+	}
+
+	fmt.Fprintln(w, "Card scores by type: \t\t")
+
+	for _, cardType := range AllCardTypes {
+		fmt.Fprintf(w, "\t\t\t\n%s: \t\t\n", cardType)
+		cardsForType := allCards.GetCardsOfType(cardType)
+		printCardScoresForType(w, cardsForType)
+	}
+
+	w.Flush()
+}
+
+
+type ByScore []*Card
+
+func (bs ByScore) Len() int {
+	return len(bs)
+}
+
+func (bs ByScore) Less(i, j int) bool {
+	return bs[i].Score < bs[j].Score
+}
+
+func (bs ByScore) Swap(i, j int) {
+	bs[i], bs[j] = bs[j], bs[i]
+}
+
+func printCardScoresForPlayer(w io.Writer, cards []*Card) {
+	printCardScores(w, cards, 10000, false)
+}
+
+func printCardScoresForType(w io.Writer, cards []*Card) {
+	printCardScores(w, cards, 15, true)
+}
+
+func printCardScores(w io.Writer, cards []*Card, max int, includeOwner bool) {
+	cardsByScore := ByScore(cards)
+	sort.Sort(sort.Reverse(&cardsByScore))
+
+	for i, card := range cardsByScore {
+		if (includeOwner) {
+			owner := ""
+			bench := ""
+			if (card.Ownership.OnBench) {
+				bench = (" (Bench)")
+			}
+			if (card.Ownership.Owner != "") {
+				owner = string(card.Ownership.Owner)
+			}
+
+			if (owner == "" && card.Score == 0) {
+				continue
+			}
+
+			fmt.Fprintf(w, "   %v \t%d\t%v%v\n", card.Name, card.Score, owner, bench)
+		} else {
+			fmt.Fprintf(w, "   %v \t%d\n", card.Name, card.Score)
+		}
+
+		if i >= max {
+			break
+		}
+	}
+}
+
