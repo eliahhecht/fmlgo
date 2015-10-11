57e7bb93c92b62d9ce9881afb6f07927b7800749
Eliah Hecht <eliahhecht@gmail.com>
2015-10-11 09:12:12 -0400

    fix a printf error

--- main/validation.go
+++ main/validation.go
@@ -7,7 +7,7 @@ func confirmCardsAreLegal(players []*Player, legalCards *CardCollection) {
 		for _, card := range append(player.Cards, player.Bench...) {
 			if !legalCards.Contains(card.Name) {
 				panic(fmt.Sprintf("%s's card %s not found in any legal set (%v legal cards)",
-					player.Name, card, len(legalCards.CardsByName)))
+					player.Name, card.Name, len(legalCards.CardsByName)))
 			}
 		}
 	}
